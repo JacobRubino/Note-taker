39ca14803c2b4102df92f1b58291d01362aa9168
Jacob Rubino <jacobrubino@gmail.com>
2023-06-18 00:08:57 -0400

    delete has more functionality, still gets an error

--- routes/routes.js
+++ routes/routes.js
@@ -1,54 +1,70 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "..", "db", "db.json");
 
-router.get('/notes', (req, res) => {
-  const filePath = path.join(__dirname, '..', 'db', 'db.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+router.get("/notes", (req, res) => {
+//get the notes from db
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Server error');
+      return res.status(500).send("Server error");
     }
-
+    //parse the notes, return them where they can be rendered on the page
     const notes = JSON.parse(data);
     res.status(200).json(notes);
   });
 });
 
-// router.post('/notes', (req, res) => {
-//   //get the object
-//   const noteData = fs.readFile('../db/db.json', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//     notes= JSON.parse(data)
+router.post("/notes", (req, res) => {
+  //get the notes from db
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Server error");
+    }
+    //parse the notes
+    const notes = JSON.parse(data);
+    //make sure the note exists, if it does push the new note thats getting posted onto the parsed list
+    if (req.body) {
+      const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Date.now().toString(),
+      };
+      notes.push(newNote);
+      // rewrite the db with the new note inside
+      fs.writeFile(filePath, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Server error");
+        }
+        // 201 because new item created
+        res.status(201).json(newNote);
+      });
+    }
+  });
+});
+.filter((note) => note !== null); // Remove the null entries
 
-//     if (req.body) {
-//       const newNote = {
-//           title: req.body.title,
-//           text: req.body.text,
-//           id: Date.now().toString(),
-//       };
-//       notes.push(newNote)
-//       fs.writeFile('../db/db.json', JSON.stringify(noteData), (err) => {
-//         if (err) {
-//           console.error(err);
-//         }
-//       });
-//     };
-//   });
-// });
-
-// router.delete('/notes:id', (req, res) =>{
-//   const deleteNote = req.params.id;
-//   console.log(deleteNote)
-//   fs.readFile('../db/db.json', (err, data) =>{
-//     if (err) throw err;
-//     console.log(data);
-//     parseData = JSON.parse(data)
-//     postDeleteNotes = JSON.stringify(parseData.filter((note)=> note.id !== deleteNote))
-//     fs.writeFile('../db/db.json', postDeleteNotes)
-//   });
-
-// });
+router.delete("/notes:id", (req, res) => {
+  // take the id of the note that we are requesting to delete
+  const deleteNote = req.params.id;
+  // get the notes from the db
+  fs.readFile("../db/db.json", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    //parse the info in the db
+    parseData = JSON.parse(data);
+    // map the notes, if any items in the note match the note id, set to null to delete
+    const postDeletedNotes = notes.map((note) => {
+      if (note.id === noteId) {
+        return null; // Mark the note for deletion by returning null
+      }
+      return note;
+    })
+    fs.writeFile("../db/db.json", JSON.stringify(postDeleteNotes));
+  });
+});
 module.exports = router;
